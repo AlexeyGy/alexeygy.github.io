2fe7bb48b277ada83eb97f97cd69b5a66746c92d
Alexey Stern <alexey.gyoeri@gmail.com>
2024-02-22 17:55:51 +0100

    Re-created blog using raw html/js/css with just a slim external markdown parser for convenience.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+generateHeader();
+generateFooter();
+
+
+//--- function definitions ---//
+function generateFooter() {
+    document.addEventListener('DOMContentLoaded', function () {
+        fetch('footer.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('footer-placeholder').innerHTML = data;
+            });
+    });
+}
+
+function generateHeader() {
+    document.addEventListener('DOMContentLoaded', function () {
+        fetch('menu.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('nav-placeholder').innerHTML = data;
+            });
+    });
+}
